1066f782060145e16fea69a2a1662c1db13401d2
Juan <Zombay338@gmail.com>
2023-10-26 11:23:54 -0600

    para jenkins

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+function suma (){
+
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const operador = document.querySelector('input[name="operador"]:checked'); 
+  
+    if(!operador){
+        Swal.fire("Error", "Debes seleccionar un operador", "error");
+    }else if(!num1 && !num2){
+        Swal.fire("Error", "Los campos deben estar completos", "error");
+    }else if(!num1 || !num2){
+        Swal.fire("Error", "Los campos deben estar completos", "error");
+    }else{
+
+        switch(operador.id){
+            case "suma":
+                result = num1 + num2;
+            break;
+            
+            case "resta":
+                result = num1 - num2;
+            break;
+        
+            case "multiplicacion":
+                result = num1 * num2;
+            break;
+    
+            case "division":
+                result = num1 / num2;
+            break
+        }
+        Swal.fire("El resultado es: ", "" + result, "success");
+    }
+}
